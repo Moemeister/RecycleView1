14678666f229f0baa10f62291169c60ecaea9ed8
Moemeister <00062716@uca.edu.sv>
2018-04-23 15:32:47 -0600

    Added a Toast

--- app/src/main/java/com/moesystems/recycleview/MainActivity.java
+++ app/src/main/java/com/moesystems/recycleview/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,12 +33,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void prepareSeries() {
+
         String TAG = "Mensaje";
         series = new ArrayList<>();
-        series.add(new Serie("The Walking Dead","13", R.drawable.twd,"Tv show"));
-        series.add(new Serie("Games of Thrones","13", R.drawable.got,"Tv show"));
-        series.add(new Serie("Breaking Bad","13", R.drawable.bb,"Tv show"));
+        series.add(new Serie("The Walking Dead","13", R.drawable.twd,"Tv show 1"));
+        series.add(new Serie("Games of Thrones","13", R.drawable.got,"Tv show 2"));
+        series.add(new Serie("Breaking Bad","13", R.drawable.bb,"Tv show 3"));
 
+
+    }
+    public void btnView(View v){
+        int size = series.size();
+        if (series.get(0).getName() == "The Walking Dead")
+        Toast.makeText(getApplicationContext(),series.get(0).getDesc(),Toast.LENGTH_SHORT).show();
+        if (series.get(1).getName() == "Games of Thrones")
+            Toast.makeText(getApplicationContext(),series.get(1).getDesc(),Toast.LENGTH_SHORT).show();
+        if (series.get(2).getName() == "Breaking Bad")
+            Toast.makeText(getApplicationContext(),series.get(2).getDesc(),Toast.LENGTH_SHORT).show();
     }
 
 
